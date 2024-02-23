9d0b13a77f62fa3c09eff7b999c7c35493d98a17
Eriseld1 <159956782+Eriseld1@users.noreply.github.com>
2024-02-22 19:30:35 -0500

    Updated the Search function with API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.temperature.current;
+	let cityElement = document.querySelector("#city");
+
+	cityElement.innerHTML = response.data.city;
+	temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+	let apiKey = "038b4b87bf0ba9bc4t1cfabo6c3435d9";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=038b4b87bf0ba9bc4t1cfabo6c3435d9&units=imperial`;
+	axios.get(apiUrl).then(updateWeather);
+}
+
 function search(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-input");
-	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = searchInput.value;
+	searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
